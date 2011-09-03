fa30a0523428dcd2a77cf17341b8d8cf752b0bb1
pasuwatw@gmail.com <pasuwatw@gmail.com@30d53804-822b-f4e2-35eb-23b76ff75910>
2011-09-03 13:41:54 +0000

    Migration Script For Jobs Order and ใบส่งของ

---
+++ tags/JoborderToSO/migration/oracle/201108061530_jobs_order_message_and_report_oracle.sql
@@ -0,0 +1,25 @@
+-- Aug 6, 2011 3:26:44 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AccessLevel,CopyFromProcess,Created,CreatedBy,EntityType,IsActive,IsBetaFunctionality,IsDirectPrint,IsReport,IsServerProcess,Name,ShowHelp,Statistic_Count,Statistic_Seconds,Updated,UpdatedBy,Value) VALUES (0,0,80007,'1','N',TO_DATE('2011-08-06 15:26:44','YYYY-MM-DD HH24:MI:SS'),100,'pasta','Y','N','N','Y','N','ใบส่งของช่าง','Y',0,0,TO_DATE('2011-08-06 15:26:44','YYYY-MM-DD HH24:MI:SS'),100,'shipment')
+;
+
+-- Aug 6, 2011 3:26:44 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Trl (AD_Language,AD_Process_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Process_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_ID=80007 AND NOT EXISTS (SELECT * FROM AD_Process_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_ID=t.AD_Process_ID)
+;
+
+-- Aug 6, 2011 3:27:17 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Attachment (AD_Attachment_ID,AD_Client_ID,AD_Org_ID,AD_Table_ID,Created,CreatedBy,IsActive,Record_ID,TextMsg,Title,Updated,UpdatedBy) VALUES (80004,0,0,284,TO_DATE('2011-08-06 15:26:51','YYYY-MM-DD HH24:MI:SS'),100,'Y',80007,NULL,'zip',TO_DATE('2011-08-06 15:26:51','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Aug 6, 2011 3:29:51 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET JasperReport='attachment:shipmentFrm.jrxml',Updated=TO_DATE('2011-08-06 15:29:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=80007
+;
+
+-- Aug 6, 2011 3:30:10 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_PrintFormat SET JasperProcess_ID=80007,Updated=TO_DATE('2011-08-06 15:30:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormat_ID=104
+;
+
---
+++ tags/JoborderToSO/migration/postgresql/201107241649_jobs_order_message_and_report_postgresql.sql
@@ -0,0 +1,50 @@
+-- Jul 24, 2011 4:23:51 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,80000,0,TO_TIMESTAMP('2011-07-24 16:23:50','YYYY-MM-DD HH24:MI:SS'),100,'pasta','Y','No Jobs Order','E',TO_TIMESTAMP('2011-07-24 16:23:50','YYYY-MM-DD HH24:MI:SS'),100,'NoSelectedJobsOrder')
+;
+
+-- Jul 24, 2011 4:23:51 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=80000 AND NOT EXISTS (SELECT * FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- Jul 24, 2011 4:27:14 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,80001,0,TO_TIMESTAMP('2011-07-24 16:27:14','YYYY-MM-DD HH24:MI:SS'),100,'pasta','Y','Jobs Order Is Not Completed','E',TO_TIMESTAMP('2011-07-24 16:27:14','YYYY-MM-DD HH24:MI:SS'),100,'JobsOrderIsNotCompleted')
+;
+
+-- Jul 24, 2011 4:27:14 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=80001 AND NOT EXISTS (SELECT * FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- Jul 24, 2011 4:35:47 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,80002,0,TO_TIMESTAMP('2011-07-24 16:35:47','YYYY-MM-DD HH24:MI:SS'),100,'pasta','Y','Cannot Save Sales Order','E',TO_TIMESTAMP('2011-07-24 16:35:47','YYYY-MM-DD HH24:MI:SS'),100,'CannotSaveSO')
+;
+
+-- Jul 24, 2011 4:35:47 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=80002 AND NOT EXISTS (SELECT * FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- Jul 24, 2011 4:42:59 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,80003,0,TO_TIMESTAMP('2011-07-24 16:42:59','YYYY-MM-DD HH24:MI:SS'),100,'pasta','Y','Jobs Order Was Generated To Sales Order.','E',TO_TIMESTAMP('2011-07-24 16:42:59','YYYY-MM-DD HH24:MI:SS'),100,'JobsOrderWasGeneratedToSO')
+;
+
+-- Jul 24, 2011 4:42:59 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=80003 AND NOT EXISTS (SELECT * FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- Jul 24, 2011 4:49:07 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,80004,0,TO_TIMESTAMP('2011-07-24 16:49:07','YYYY-MM-DD HH24:MI:SS'),100,'pasta','Y','Cannot Save Jobs Order','E',TO_TIMESTAMP('2011-07-24 16:49:07','YYYY-MM-DD HH24:MI:SS'),100,'CannotSaveJobsOrder')
+;
+
+-- Jul 24, 2011 4:49:08 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=80004 AND NOT EXISTS (SELECT * FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
