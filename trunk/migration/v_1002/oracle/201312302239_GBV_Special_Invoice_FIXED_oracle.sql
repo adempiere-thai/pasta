4a8a33d14e6efaf19bd8feed3b56388b85d6427f
pasuwatw@gmail.com <pasuwatw@gmail.com@30d53804-822b-f4e2-35eb-23b76ff75910>
2013-12-30 15:40:57 +0000

    FIXED : GBV Special Invoice Process

---
+++ trunk/migration/v_1002/oracle/201312302239_GBV_Special_Invoice_FIXED_oracle.sql
@@ -0,0 +1,141 @@
+-- Dec 30, 2013 10:25:25 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='GBV_สร้างใบแจ้งหนี้(ลูกค้า)ด้วยราคาพิเศษ',Updated=TO_DATE('2013-12-30 22:25:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=80024
+;
+
+-- Dec 30, 2013 10:25:25 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='N' WHERE AD_Process_ID=80024
+;
+
+-- Dec 30, 2013 10:25:25 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='GBV_สร้างใบแจ้งหนี้(ลูกค้า)ด้วยราคาพิเศษ',Updated=TO_DATE('2013-12-30 22:25:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=80047
+;
+
+-- Dec 30, 2013 10:25:25 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=80047
+;
+
+-- Dec 30, 2013 10:26:12 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET EntityType='pasta',Updated=TO_DATE('2013-12-30 22:26:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=80095
+;
+
+-- Dec 30, 2013 10:26:44 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET SeqNo=45,Updated=TO_DATE('2013-12-30 22:26:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=80092
+;
+
+-- Dec 30, 2013 10:26:59 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET SeqNo=20,Updated=TO_DATE('2013-12-30 22:26:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=80093
+;
+
+-- Dec 30, 2013 10:27:04 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET SeqNo=30,Updated=TO_DATE('2013-12-30 22:27:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=80094
+;
+
+-- Dec 30, 2013 10:27:11 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET SeqNo=40,Updated=TO_DATE('2013-12-30 22:27:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=80092
+;
+
+-- Dec 30, 2013 10:28:35 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET DefaultValue='1000010',Updated=TO_DATE('2013-12-30 22:28:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=80094
+;
+
+-- Dec 30, 2013 10:29:04 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Description='Generate Invoice with special price *For organization: “บริษัท โกลบอล วินยาร์ด จำกัด” only* ',Updated=TO_DATE('2013-12-30 22:29:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=80024
+;
+
+-- Dec 30, 2013 10:29:04 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='N' WHERE AD_Process_ID=80024
+;
+
+-- Dec 30, 2013 10:29:04 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description='Generate Invoice with special price *For organization: “บริษัท โกลบอล วินยาร์ด จำกัด” only* ', IsActive='Y', Name='GBV_สร้างใบแจ้งหนี้(ลูกค้า)ด้วยราคาพิเศษ',Updated=TO_DATE('2013-12-30 22:29:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=80047
+;
+
+-- Dec 30, 2013 10:29:04 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=80047
+;
+
+-- Dec 30, 2013 10:30:02 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Description=NULL, Help='Generate Invoice with special price *For organization: “บริษัท โกลบอล วินยาร์ด จำกัด” only* ',Updated=TO_DATE('2013-12-30 22:30:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=80024
+;
+
+-- Dec 30, 2013 10:30:02 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='N' WHERE AD_Process_ID=80024
+;
+
+-- Dec 30, 2013 10:30:02 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='GBV_สร้างใบแจ้งหนี้(ลูกค้า)ด้วยราคาพิเศษ',Updated=TO_DATE('2013-12-30 22:30:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=80047
+;
+
+-- Dec 30, 2013 10:30:02 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=80047
+;
+
+-- Dec 30, 2013 10:30:41 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Description='Generate Invoice with special price', Help='*For organization: “บริษัท โกลบอล วินยาร์ด จำกัด” only* ',Updated=TO_DATE('2013-12-30 22:30:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=80024
+;
+
+-- Dec 30, 2013 10:30:41 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='N' WHERE AD_Process_ID=80024
+;
+
+-- Dec 30, 2013 10:30:41 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description='Generate Invoice with special price', IsActive='Y', Name='GBV_สร้างใบแจ้งหนี้(ลูกค้า)ด้วยราคาพิเศษ',Updated=TO_DATE('2013-12-30 22:30:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=80047
+;
+
+-- Dec 30, 2013 10:30:41 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=80047
+;
+
+-- Dec 30, 2013 10:31:38 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET Name='To Tax code',Updated=TO_DATE('2013-12-30 22:31:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=80092
+;
+
+-- Dec 30, 2013 10:31:38 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para_Trl SET IsTranslated='N' WHERE AD_Process_Para_ID=80092
+;
+
+-- Dec 30, 2013 10:35:51 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Val_Rule (AD_Client_ID,AD_Org_ID,AD_Val_Rule_ID,Code,Created,CreatedBy,EntityType,IsActive,Name,Type,Updated,UpdatedBy) VALUES (0,0,80011,'(EXISTS ( SELECT * FROM C_PeriodControl pc WHERE C_Period.C_Period_ID = pc.C_Period_ID AND pc.PeriodStatus = ''O'') OR EXISTS ( SELECT * FROM C_AcctSchema a, C_Period p WHERE C_Period.C_Period_ID = p.C_Period_ID AND a.AutoPeriodControl = ''Y'' AND ( (p.StartDate BETWEEN SYSDATE - a.Period_OpenHistory AND SYSDATE + a.Period_OpenFuture) OR (p.EndDate BETWEEN SYSDATE - a.Period_OpenHistory AND SYSDATE + a.Period_OpenFuture))))
+ORDER BY C_Period.StartDate',TO_DATE('2013-12-30 22:35:51','YYYY-MM-DD HH24:MI:SS'),100,'pasta','Y','C_Period Open','S',TO_DATE('2013-12-30 22:35:51','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Dec 30, 2013 10:36:03 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Val_Rule SET Name='C_Period Open (Order By StartDate)',Updated=TO_DATE('2013-12-30 22:36:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=80011
+;
+
+-- Dec 30, 2013 10:36:14 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET AD_Val_Rule_ID=80011,Updated=TO_DATE('2013-12-30 22:36:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=80091
+;
+
+-- Dec 30, 2013 10:37:33 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET AD_Reference_ID=18, AD_Reference_Value_ID=233, AD_Val_Rule_ID=NULL,Updated=TO_DATE('2013-12-30 22:37:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=80091
+;
+
