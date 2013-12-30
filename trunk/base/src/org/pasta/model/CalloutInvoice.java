2c3e09718a3ac28998e105488253d912396d4ce6
pasuwatw@gmail.com <pasuwatw@gmail.com@30d53804-822b-f4e2-35eb-23b76ff75910>
2013-12-30 15:00:08 +0000

    Change Callout Invoice

--- trunk/base/src/org/pasta/model/CalloutInvoice.java
+++ trunk/base/src/org/pasta/model/CalloutInvoice.java
@@ -10,7 +10,9 @@ import org.compiere.model.CalloutEngine;
 import org.compiere.model.GridField;
 import org.compiere.model.GridTab;
 import org.compiere.model.MDocType;
+import org.compiere.model.MInvoiceLine;
 import org.compiere.util.DB;
+import org.compiere.util.Msg;
 
 public class CalloutInvoice extends CalloutEngine {
 	public String docType(Properties ctx, int WindowNo, GridTab mTab,
@@ -43,10 +45,11 @@ public class CalloutInvoice extends CalloutEngine {
 		int AD_Client_ID = (Integer)mTab.getValue("AD_Client_ID");
 		int AD_Org_ID = (Integer)mTab.getValue("AD_Org_ID");
 
-		String sql = "SELECT ivt.datetaxinvoice , iv.grandtotal , iv.C_Invoice_ID "
+		String sql = "SELECT ivt.datetaxinvoice , case ivt.actualtaxbaseamt when 0 then ivt.taxbaseamt else ivt.actualtaxbaseamt end taxbaseamt , iv.C_Invoice_ID , iv.C_BPartner_ID "
 				+ "FROM c_invoice iv ,c_invoicetax ivt "
 				+ "WHERE iv.c_invoice_id = ivt.c_invoice_id "
-				+ "AND iv.ispaid = 'Y' "
+				//+ "AND iv.ispaid = 'Y' "
+				+ "AND iv.DocStatus = 'CO' "
 				+ "AND iv.ad_client_id = ? AND iv.ad_org_id = ?  " 
 				+ "AND ivt.taxinvoiceno = ? AND iv.issotrx = ? ";
 
@@ -63,13 +66,17 @@ public class CalloutInvoice extends CalloutEngine {
 				Timestamp dateTaxInvoice = rs.getTimestamp(1);
 				BigDecimal grandTotal = rs.getBigDecimal(2);
 				int C_Invoice_ID = rs.getInt(3);
+				int C_BPartner_ID = rs.getInt(4);
 				
 				mTab.setValue("RefTaxInvoiceDate", dateTaxInvoice);
 				mTab.setValue("RefTaxInvoiceAmt", grandTotal);
 				mTab.setValue("Ref_Invoice_ID", C_Invoice_ID);
+				
+				mTab.setValue("C_BPartner_ID", C_BPartner_ID);
 			}
 			else{
 				mTab.setValue("Ref_Invoice_ID", null);
+				return "Cannot Found Tax Invoice No. "+taxInvoiceNo;
 			}
 		} catch (Exception ex) {
 			return ex.getMessage();
@@ -77,4 +84,32 @@ public class CalloutInvoice extends CalloutEngine {
 
 		return "";
 	}
+	
+	public String productReferenceInvoiceLine(Properties ctx, int WindowNo, GridTab mTab,
+			GridField mField, Object value) {
+		if(value == null){
+			return "";
+		}
+		
+		int C_InvoiceLine_ID = (Integer)value;
+		
+		MInvoiceLine invoiceLine = new MInvoiceLine(ctx,C_InvoiceLine_ID,null);
+		
+		if(invoiceLine != null){
+			if(invoiceLine.getM_Product_ID() != 0 ){
+				mTab.setValue("M_Product_ID",invoiceLine.getM_Product_ID());
+				mTab.setValue("C_Charge_ID", null);
+			}
+			else if(invoiceLine.getC_Charge_ID() != 0){
+				mTab.setValue("C_Charge_ID", invoiceLine.getC_Charge_ID());
+				mTab.setValue("M_Product_ID",null);
+			}
+		}
+		else{
+			return Msg.translate(ctx, "NOT_FOUND_REF_INVOICE_LINE");
+		}
+		
+		return "";
+	}
+	
 }
